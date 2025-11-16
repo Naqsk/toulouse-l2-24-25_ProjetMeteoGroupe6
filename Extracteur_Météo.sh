e25b0efaba92a3cb1169a01d5e8fe37d8b2d364a
Noah <noah.goize@gmail.com>
2025-11-16 20:08:15 +0100

    V1.2 Implémenter la récupération de données brutes avec curl.

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -8,3 +8,11 @@ then
 else
     ville=$1
 fi
+
+#exportation données brutes dans fichier .txt
+curl -s wttr.in/$ville?format=j2 > local.txt
+if [ $? -ne 0 ]
+then
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - Erreur de connexion à wttr.in pour $ville lors de l'exportation des données brutes" >> meteo_error.log
+    exit 1
+fi
