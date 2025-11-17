9e6df4197d144644cfa838a8693e62e62b11a378
Anis <anis.mohamadi@toulouse.miage.fr>
2025-11-17 10:46:20 +0100

    V1.3 - extraction de température actuelle et lendemain

--- Extracteur_Météo.sh
+++ Extracteur_Météo.sh
@@ -16,3 +16,19 @@ then
     echo "$(date +"%Y-%m-%d %H:%M:%S") - Erreur de connexion à wttr.in pour $ville lors de l'exportation des données brutes" >> meteo_error.log
     exit 1
 fi
+
+#récupération de la température actuelle
+tempact=$(curl -s wttr.in/$ville?format="%t")
+if [ $? -ne 0 ]
+then
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - Erreur de connexion à wttr.in pour $vi>
+    exit 1
+fi
+
+if [ "${tempact:0:1}" = "+" ]
+then
+    tempact=$(echo $tempact | cut -c 2-)
+fi
+
+#récupération de la température du lendemain
+templen=$(head -n 103 local.txt | tail -n 1 | grep -oE "\-*[0-9]*")°C
